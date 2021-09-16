b05e948e96510d3011607bf117172508174e8cd4
revanthkumaar <iamrevanthkumar@gmail.com>
2021-09-16 10:13:01 +0530

    lakjdlaksj

---
+++ src/pages/home-page/home-page.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home-page.component.css'
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+                <p>this is home page</p>
+        </div>
+    )
+}
+
+export default HomePage;
+
