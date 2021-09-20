c8d1a201563aff14afa94cac10b5a8c6a37e0909
revanthkumaar <iamrevanthkumar@gmail.com>
2021-09-20 16:00:45 +0530

    falskjdl
    kjqlaksjd

--- src/pages/home-page/home-page.component.js
+++ src/pages/home-page/home-page.component.js
@@ -4,15 +4,23 @@ import Directory from "../../components/directory/directory.component.jsx";
 import Header from "../../components/header/header.component";
 import data from '../../data.json'
 import ToDoList from "../../components/todolist/todolist.component";
+import ToDoForm from '../../components/todoForm/todoForm';
 
 const HomePage = () => {
   const [toDoList, setToDoList] = useState(data);
+
+  const addTask = (userInput) => {
+    let copy = [...toDoList];
+    copy = [...copy, {id: toDoList.length+1, task: userInput, complete:false}];
+    setToDoList(copy)
+  }
 
     return (
       <div className="home-page">
         <Header />
         <Directory />
         <ToDoList toDoList={toDoList} />
+        <ToDoForm addTask={addTask} />
       </div>
     );
 }
